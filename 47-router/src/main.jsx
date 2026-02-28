e7ad9c6e4644986c0be037768f5af98e94dad6cd
soumen kumar bera <soumenbera4017@gmail.com>
2026-02-28 16:33:29 +0530

    layout react router dom

--- 47-router/src/main.jsx
+++ 47-router/src/main.jsx
@@ -2,8 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+import App from "./routes/App.jsx";
+import PostList from "./components/PostList.jsx";
+import CreatePost from "./components/CreatePost.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <CreatePost />,
+      },
+      { path: "/create-post", element: <PostList /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- 47-router/src/routes/App.jsx
+++ 47-router/src/routes/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Footer from "./components/footer";
-import Sidebar from "./components/sidebar";
-import CreatePost from "./components/CreatePost";
-import PostList from "./components/PostList";
+import Header from "../components/Header";
+import Footer from "../components/footer";
+import Sidebar from "../components/sidebar";
+import CreatePost from "../components/CreatePost";
+import PostList from "../components/PostList";
 import { useState } from "react";
-import PostListProvider from "./store/post-list-store";
-
+import PostListProvider from "../store/post-list-store";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [selectedTap, setSelectedTap] = useState();
@@ -21,12 +21,7 @@ function App() {
         ></Sidebar>
         <div className="content">
           <Header></Header>
-
-          {selectedTap == "Home" ? (
-            <PostList></PostList>
-          ) : (
-            <CreatePost></CreatePost>
-          )}
+          <Outlet/>
 
           <Footer></Footer>
         </div>
